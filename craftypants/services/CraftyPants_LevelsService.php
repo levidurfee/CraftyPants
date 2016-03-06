21513e6d917dd34717fe75f3c36621e2dbff1093
Levi Durfee <levi.durfee@gmail.com>
2016-03-05 21:55:25 -0500

    adding another method to the chain

--- craftypants/services/CraftyPants_LevelsService.php
+++ craftypants/services/CraftyPants_LevelsService.php
@@ -6,6 +6,6 @@ class CraftyPants_LevelsService extends BaseApplicationComponent
     public function getLevels($name = 'levi')
     {
         $q = craft()->db->createCommand();
-        return $q->from('craftypants_levels')->where(array('name' => $name));
+        return $q->from('craftypants_levels')->where(array('name' => $name))->queryAll();
     }
 }
