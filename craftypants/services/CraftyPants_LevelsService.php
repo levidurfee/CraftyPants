95b19fbe35bd32f92cf226b79a6b878b310a10f8
Levi Durfee <levi.durfee@gmail.com>
2016-03-05 21:41:07 -0500

    running a query, docs are not clear on how to return the data

---
+++ craftypants/services/CraftyPants_LevelsService.php
@@ -0,0 +1,11 @@
+<?php
+namespace Craft;
+
+class CraftyPants_LevelsService extends BaseApplicationComponent
+{
+    public function getLevels($name = 'levi')
+    {
+        $q = craft()->db->createCommand();
+        return $q->from('craftypants_levels')->where('name' => $name);
+    }
+}
