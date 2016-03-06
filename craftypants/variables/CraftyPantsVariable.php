b3be101dc5ce48a08c3cb6da07707ff96efb6360
Levi Durfee <levi.durfee@gmail.com>
2016-03-05 19:58:36 -0500

    Adding variables

---
+++ craftypants/variables/CraftyPantsVariable.php
@@ -0,0 +1,15 @@
+<?php
+namespace levidurfee;
+
+class CraftyPantsVariable
+{
+    public function getCrafty()
+    {
+        // Return crafty level
+    }
+
+    public function getPants()
+    {
+        // Return pants level
+    }
+}
